ebf1128a7fd9966d90059c9638b94fbc89e40b45
wikto398 <wikto398@gmail.com>
2026-04-20 02:39:10 +0200

    Minor fixes in Dijkstra

--- Pathing/Pathing.gd
+++ Pathing/Pathing.gd
@@ -34,24 +34,36 @@ static func dijkstra(start: Field, max_range: int, field_grid: FieldGrid, condit
 	var open_set: PriorityQueue = PriorityQueue.new()
 	var came_from: Dictionary[Field, Field] = {}
 	var g_score: Dictionary[Field, int] = {}
+	var visited: Dictionary[Field, bool] = {}
 
 	g_score[start] = 0
 	open_set.push(start, g_score[start])
 
 	var reachable_fields: Array[Field] = []
+
 	while not open_set.is_empty():
 		var current: Field = open_set.pop()
+
+		if visited.has(current):
+			continue
+		visited[current] = true
+
 		if g_score[current] > max_range:
 			continue
+
 		reachable_fields.append(current)
+
 		for neighbor in field_grid.get_neighbours(current.grid_position):
 			if not condition.evaluate({"neighbor": neighbor, "unit": unit}):
 				continue
+
 			var tentative_g_score = g_score[current] + neighbor.movement_cost
+
 			if not g_score.has(neighbor) or tentative_g_score < g_score[neighbor]:
 				came_from[neighbor] = current
 				g_score[neighbor] = tentative_g_score
 				open_set.push(neighbor, g_score[neighbor])
+
 	return reachable_fields
 
 static func _reconstruct_path(came_from: Dictionary[Field, Field], current: Field) -> Array[Field]:
