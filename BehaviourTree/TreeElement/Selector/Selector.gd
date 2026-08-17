78c3f989f3bcad30455de281604f715ffe11d256
wikto398 <wikto398@gmail.com>
2026-08-17 17:10:12 +0200

    Removed print due to unnecessary spam

--- BehaviourTree/TreeElement/Selector/Selector.gd
+++ BehaviourTree/TreeElement/Selector/Selector.gd
@@ -4,7 +4,6 @@ class_name Selector extends TreeElement
 
 func _do_tick(blackboard: Blackboard) -> Status:
     for child in children:
-        DebugLogger.info("Selector: Ticking child element: " + str(child))
         var result = await child.tick(blackboard)
         if result == Status.SUCCESS:
             return Status.SUCCESS
